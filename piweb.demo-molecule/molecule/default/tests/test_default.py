686d07fcbd2824624279326153abc705cc6e0d0e
Julian Vanden Broeck <julian.vandenbroeck@unamur.be>
2019-05-24 00:10:04 +0200

    Ajout de 3 tests pour httpd
      - Verif que le package est bien installé
      - Verif que le service tourne bien et est actif (se lance au boot)
      - Verif que le port TCP 80 est bien en ecoute

--- piweb.demo-molecule/molecule/default/tests/test_default.py
+++ piweb.demo-molecule/molecule/default/tests/test_default.py
@@ -1,3 +1,4 @@
+#!/bin/venv python3
 import os
 
 import testinfra.utils.ansible_runner
@@ -6,9 +7,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_httpd_is_installed(host):
+    pkg = host.package('httpd')
+    assert pkg.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_httpd_running_and_enabled(host):
+    httpd = host.service("httpd")
+    assert httpd.is_running
+    assert httpd.is_enabled
+
+
+def test_port_80_listening(host):
+    port_80 = host.socket("tcp://0.0.0.0:80")
+    assert port_80.is_listening
